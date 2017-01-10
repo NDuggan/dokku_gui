374794604643c64abf5c51cf827475f04475881f
Niall Duggan <nduggan91@gmail.com>
2017-01-10 12:52:29 +0000

    Add custom flash types and the ability to render them

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def flash_class(type)
+    case type.to_sym
+    when :error, :failure, :danger     then 'danger'
+    when :info                         then 'info'
+    when :notice, :success             then 'success'
+    when :alert, :warning              then 'warning'
+    else                               type.to_s
+    end
+  end
 end
