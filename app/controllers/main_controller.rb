5d0b4b55f76ccc19525c415e2a5ab620a7d2c97a
Niall Duggan <nduggan91@gmail.com>
2017-01-10 12:53:22 +0000

    Add CPU, RAM, etc. to the dashboard

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,27 +1,10 @@
 class MainController < ApplicationController
   def dashboard
-
-  end
-
-  def apps
-    @output = []
     ssh_connect do |ssh|
-     result = ssh.exec!('dokku apps')
-     result.split("\n").each_with_index do |app, i|
-       next if i == 0
-       @output << app
-     end
-    end
-  end
-
-  def list
-    @output = []
-    ssh_connect do |ssh|
-     result = ssh.exec!('dokku ls')
-     result.split("\n").each_with_index do |app, i|
-       next if i == 0
-       @output << app.split(" ").flatten
-     end
+      @cpu = ssh.exec!('cat /proc/cpuinfo | grep processor').split("\n").last.match(/processor\t:(\S*) (.*)/)[2].to_i + 1
+      @memory = ssh.exec!('cat /proc/meminfo | grep MemTotal:').match(/^MemTotal:(\S*) (.*)/)[2].to_i * 1024
+      @docker_version = ssh.exec!('docker --version').match(/^Docker version(\S*) (.*)/)[2]
+      @dokku_version = ssh.exec!('dokku version')
     end
   end
 end
