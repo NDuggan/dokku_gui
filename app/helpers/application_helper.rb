a3a51723edcecd0e14f6f33103537e45eb52a2a3
Niall Duggan <niall@yellowstone.co.uk>
2017-01-11 23:15:36 +0000

    Further main layout style improvements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,22 @@
 module ApplicationHelper
+  def is_active_controller(*controller_names)
+    controller_names.each do |controller_name|
+      if params[:controller] == controller_name
+        return "active"
+      end
+    end
+    nil
+  end
+
+  def is_active_action(*action_names)
+    action_names.each do |action_name|
+      if params[:action] == action_name
+        return "active"
+      end
+    end
+    nil
+  end
+  
   def flash_class(type)
     case type.to_sym
     when :error, :failure, :danger     then 'danger'
